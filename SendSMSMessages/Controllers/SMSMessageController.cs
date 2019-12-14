122405294ebd463019cfe473139ae7987dd33d9a
krtkrm <keremkurt@sakusoft.com>
2019-12-14 16:12:18 +0300

    sa
    as

---
+++ SendSMSMessages/Controllers/SMSMessageController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Nexmo.Api;
+using SendSMSMessages.ViewModels;
+
+namespace SendSMSMessages.Controllers
+{
+    public class SMSMessageController : Controller
+    {
+        // GET: SMSMessage  
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult SendMessage()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SendMessage(Message message)
+        {
+            var results = SMS.Send(new SMS.SMSRequest
+            {
+                from = Configuration.Instance.Settings["appsettings:NEXMO_FROM_NUMBER"],
+                to = message.To,
+                text = message.ContentMsg
+            });
+
+
+            return View();
+        }
+    }
+}
---
+++ SendSMSMessages/Models/message.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SendSMSMessages.ViewModels
+{
+    public class Message
+    {
+        public string To { get; set; }
+        public string ContentMsg { get; set; }
+    }
+}
